908da4873bb575ef12f65abd175e2d82ff37f461
Enochsoul <43549250+Enochsoul@users.noreply.github.com>
2020-09-06 20:46:49 -0600

    Removed experimental random.org die roller from master branch, it has it's own branch now.

--- SCDBot.py
+++ SCDBot.py
@@ -6,7 +6,6 @@ import sqlite3
 from datetime import datetime, tzinfo, timedelta
 
 import discord
-import requests
 from discord.ext import commands, tasks
 from dotenv import load_dotenv, set_key
 from tabulate import tabulate
@@ -745,7 +744,7 @@ async def on_setdate_error(ctx, error):
 # =========================================================
 
 
-def die_roll_old(die_count, die_size):
+def die_roll(die_count, die_size):
     """Used for generating random numbers for dice rolling.
 
     :param die_count: Number of random integers to generate.
@@ -760,36 +759,6 @@ def die_roll_old(die_count, die_size):
     while count > 0:
         result.append(random.randint(1, die_size))
         count -= 1
-    total = sum(result)
-    bold_item = [f"**{item}**" if item == die_size else str(item) for item in result]
-    result_list = "+".join(bold_item)
-    return result_list, total
-
-
-def die_roll(die_count, die_size):
-    """Used for generating random numbers from random.org's APi for dice rolling.
-
-    :param die_count: Number of random integers to generate.
-    :type die_count: int
-    :param die_size: Max value of random number generated.
-    :type die_size: int
-    :return: List of individual results and sum of results.
-    :rtype: (list, int)
-    """
-    json_body = {
-        "jsonrpc": "2.0",
-        "method": "generateIntegers",
-        "params": {
-            "apiKey": random_org_apikey,
-            "n": die_count,
-            "min": 1,
-            "max": die_size,
-            "replacement": True
-        },
-        "id": 1337
-    }
-    response = requests.post('https://api.random.org/json-rpc/2/invoke', json=json_body)
-    result = response.json()['result']['random']['data']
     total = sum(result)
     bold_item = [f"**{item}**" if item == die_size else str(item) for item in result]
     result_list = "+".join(bold_item)
