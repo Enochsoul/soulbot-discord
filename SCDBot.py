b64030e7d3896f65931e459c1637ebc2c813bfa8
Enochsoul <43549250+Enochsoul@users.noreply.github.com>
2020-09-06 19:25:41 -0600

    Created new branch just for random.org api version of dice roller.

--- SCDBot.py
+++ SCDBot.py
@@ -745,27 +745,6 @@ async def on_setdate_error(ctx, error):
 # =========================================================
 
 
-def die_roll_old(die_count, die_size):
-    """Used for generating random numbers for dice rolling.
-
-    :param die_count: Number of random integers to generate.
-    :type die_count: int
-    :param die_size: Max value of random number generated.
-    :type die_size: int
-    :return: List of individual results and sum of results.
-    :rtype: (list, int)
-    """
-    count = die_count
-    result = []
-    while count > 0:
-        result.append(random.randint(1, die_size))
-        count -= 1
-    total = sum(result)
-    bold_item = [f"**{item}**" if item == die_size else str(item) for item in result]
-    result_list = "+".join(bold_item)
-    return result_list, total
-
-
 def die_roll(die_count, die_size):
     """Used for generating random numbers from random.org's APi for dice rolling.
 
