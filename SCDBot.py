314f855f8bf780bd7dd952610e6f92372bbd173e
Enochsoul <43549250+Enochsoul@users.noreply.github.com>
2020-09-09 16:47:00 -0600

    Experimenting with a different dice roller.

--- SCDBot.py
+++ SCDBot.py
@@ -745,8 +745,28 @@ async def on_setdate_error(ctx, error):
 # =========================================================
 
 
+rand_arrays = {
+    'd20': [random.randint(1, 20) for _ in range(1000)],
+    'd12': [random.randint(1, 12) for _ in range(1000)],
+    'd10': [random.randint(1, 10) for _ in range(1000)],
+    'd8': [random.randint(1, 8) for _ in range(1000)],
+    'd6': [random.randint(1, 6) for _ in range(1000)]}
+
+
+@tasks.loop(hours=1)
+async def array_builder():
+    global rand_arrays
+    rand_arrays = {
+        'd20': [random.randint(1, 20) for _ in range(1000)],
+        'd12': [random.randint(1, 12) for _ in range(1000)],
+        'd10': [random.randint(1, 10) for _ in range(1000)],
+        'd8': [random.randint(1, 8) for _ in range(1000)],
+        'd6': [random.randint(1, 6) for _ in range(1000)]}
+
+
 def die_roll(die_count, die_size):
-    """Used for generating random numbers from random.org's APi for dice rolling.
+    global rand_arrays
+    """Used for generating random numbers for dice rolling.
 
     :param die_count: Number of random integers to generate.
     :type die_count: int
@@ -755,20 +775,20 @@ def die_roll(die_count, die_size):
     :return: List of individual results and sum of results.
     :rtype: (list, int)
     """
-    json_body = {
-        "jsonrpc": "2.0",
-        "method": "generateIntegers",
-        "params": {
-            "apiKey": random_org_apikey,
-            "n": die_count,
-            "min": 1,
-            "max": die_size,
-            "replacement": True
-        },
-        "id": 1337
-    }
-    response = requests.post('https://api.random.org/json-rpc/2/invoke', json=json_body)
-    result = response.json()['result']['random']['data']
+    result = []
+    if die_size == 20:
+        result = random.sample(rand_arrays['d20'], die_count)
+    elif die_size == 12:
+        result = random.sample(rand_arrays['d12'], die_count)
+    elif die_size == 10:
+        result = random.sample(rand_arrays['d10'], die_count)
+    elif die_size == 8:
+        result = random.sample(rand_arrays['d8'], die_count)
+    elif die_size == 6:
+        result = random.sample(rand_arrays['d6'], die_count)
+    else:
+        for i in range(0, die_count):
+            result.append(random.randint(1, die_size))
     total = sum(result)
     bold_item = [f"**{item}**" if item == die_size else str(item) for item in result]
     result_list = "+".join(bold_item)
