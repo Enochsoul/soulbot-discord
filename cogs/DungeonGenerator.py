f081c443b57e3a014a90acef2a7ff9ad60fc60d1
Jason S <soulscode@gmail.com>
2021-08-28 23:00:59 -0600

    Dungeon creation, movement and current room look built.

--- cogs/DungeonGenerator.py
+++ cogs/DungeonGenerator.py
@@ -15,8 +15,8 @@ def embed_template(ctx, dungeon_desc: str, exits: list):
                                      description=f"{dungeon_desc}",
                                      color=0xff0000)
     dungeongen_embed.add_field(name="__Exits:__",
-                               value="\n\u200b",
-                               # value=f"Enter **{ctx.prefix}dg go <exit #>** to pick a direction.",
+                               # value="\n\u200b",
+                               value=f"Enter **{ctx.prefix}dg go <exit #>** to pick a direction.",
                                inline=False)
     for e in exits:
         dungeongen_embed.add_field(name=f"{exits.index(e)+1}) {list(e.values())[0]}",
@@ -59,12 +59,11 @@ class DungeonDelve:
                         "wall, with a heavy wooden door set into an opening in that wall. A sign mounted high on the " \
                         "wall reads:\n\t\t**Welcome to the Dungeon of the Endless.\n\t\tMay you find your fortune in " \
                         "it's ever shifting corridors.**\n"
-        entrance_exit = [{1: "Enter"}]
+        entrance_exit = [{1: "Enter the Dungeon"}]
         self.dungeon[0] = {"description": entrance_desc, "exits": entrance_exit}
         # Create all of the rooms.
         while len(self.dungeon) < room_num + 1:
             self.dungeon[len(self.dungeon)] = {"description": create_room()[0], "exits": []}
-        print('Rooms created.')
         # First pass loop to link the rooms.
         for room in self.dungeon:
             room_list = list(range(1, room_num + 1))
@@ -78,7 +77,7 @@ class DungeonDelve:
                         if room in self.dungeon[destination_room]["exits"]:
                             room_list.remove(destination_room)
                             destination_room = random.choice(room_list)
-                        if len(self.dungeon[room]["exits"]) < 4 and len(self.dungeon[destination_room]["exits"]) < 2:
+                        if len(self.dungeon[room]["exits"]) < 3 and len(self.dungeon[destination_room]["exits"]) < 2:
                             destination_door = {room: random.choice(dungeongen_db['exits'])}
                             source_door = {destination_room: e}
                             self.dungeon[room]["exits"].append(source_door)
@@ -124,12 +123,28 @@ class DungeonGen(commands.Cog, name="Dungeon Generator"):
             exits = dungeongen_obj[ctx.guild.id].dungeon[0]["exits"]
         await ctx.send(embed=embed_template(ctx, desc, exits))
 
-    @dg.commandO(help="Enter the number of the direction to go.")
-    async def go(self, direction: int):
-        directions = dungeongen_obj[ctx.guild.id].dungeon[dungeongen_obj[ctx.guild.id].current_room_id]
+    @dg.command(help="Enter the number of the direction to go.")
+    async def go(self, ctx, direction: int):
+        if len(dungeongen_obj[ctx.guild.id].dungeon) == 0:
+            await ctx.send('Please create a new dungeon first.')
+        else:
+            directions = dungeongen_obj[ctx.guild.id].dungeon[dungeongen_obj[ctx.guild.id].current_room_id]['exits']
+            chosen_exit = directions[direction - 1]
+            new_room_desc = dungeongen_obj[ctx.guild.id].dungeon[list(chosen_exit.keys())[0]]["description"]
+            new_room_exits = dungeongen_obj[ctx.guild.id].dungeon[list(chosen_exit.keys())[0]]["exits"]
+            embed = embed_template(ctx, new_room_desc, new_room_exits)
+            dungeongen_obj[ctx.guild.id].current_room_id = list(chosen_exit.keys())[0]
+            await ctx.send(f'Exiting to {list(chosen_exit.values())[0]}', embed=embed)
 
+    @dg.command(help="Look at the contents of the current room.")
+    async def look(self, ctx):
+        description = dungeongen_obj[ctx.guild.id].dungeon[dungeongen_obj[ctx.guild.id].current_room_id]["description"]
+        exits = dungeongen_obj[ctx.guild.id].dungeon[dungeongen_obj[ctx.guild.id].current_room_id]["exits"]
+        embed = embed_template(ctx, description, exits)
+        await ctx.send(embed=embed)
 
     @new.error
+    @go.error
     async def new_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Please indicate the number of rooms for the new dungeon.')
