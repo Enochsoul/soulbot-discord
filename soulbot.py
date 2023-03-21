2bddecb9f5d18768fffc92296921e65794f6a980
Jason S <soulscode@gmail.com>
2023-03-20 22:00:52 -0600

    Git noobishness, reapplying intents and dice roller case-insensitivity.

--- soulbot.py
+++ soulbot.py
@@ -8,7 +8,6 @@ from soulbot_support import soulbot_db, UTC, ET, MT, CT, PT
 
 intents = discord.Intents.default()
 intents.members = True
-intents.message_content = True
 
 with open('soulbot.conf', "r") as config:
     bot_config = json.load(config)
@@ -165,7 +164,6 @@ if __name__ == "__main__":
     bot.load_extension('DiceRoller')
     for cog in bot_config['load_cogs']:
         try:
-            print(f"Loading {cog}")
             bot.load_extension(f'cogs.{cog}')
         except commands.ExtensionNotLoaded as cog_error:
             print(f'There was a problem loading Cog {cog}.\nException:\n{cog_error}')
