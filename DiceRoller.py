fcf8aa8cef9dd2e336404c93059a7bbf49fe929b
Jason S. <soulscode@gmail.com>
2025-12-21 22:08:32 -0700

    Formatting updates made by ruff.
    Mostly spacing and quotation consistency updates.

--- DiceRoller.py
+++ DiceRoller.py
@@ -14,8 +14,8 @@ die_roll = Dice()
 
 def deck_embed_template(image_file: str):
     """Card Draw embed template."""
-    embed_template = discord.Embed(title='You drew:', color=0xFF0000)
-    embed_template.set_image(url=f'attachment://{image_file}')
+    embed_template = discord.Embed(title="You drew:", color=0xFF0000)
+    embed_template.set_image(url=f"attachment://{image_file}")
     return embed_template
 
 
@@ -26,8 +26,8 @@ class DiceRoller(discord.Cog, name='Dice Roller'):
         self.bot = bot
         self.dice_roll = die_roll
         self.card_list = {guild.id: [] for guild in bot.guilds}
-        self.active_deck = {guild.id: '' for guild in bot.guilds}
-        self.deck_list = os.listdir('./data/decks')
+        self.active_deck = {guild.id: "" for guild in bot.guilds}
+        self.deck_list = os.listdir("./data/decks")
 
     @commands.command(
         help="Dice roller.  Expected format: NdN+N.(Ex: 2d6+2)\nRoll Types: \n\td=Default\n\tad=Advantage\n\tdd=Disadvantage\n\ted=Exploding Dice\n\tdl=Drop Lowest Die\n\tdh=Drop Highest Die"
@@ -39,20 +39,17 @@ class DiceRoller(discord.Cog, name='Dice Roller'):
         except (InvalidRollType, InvalidDiceFormat) as e:
             await ctx.send(str(e))
 
-    @commands.group(help='Draw cards from a selected Deck')
+    @commands.group(help="Draw cards from a selected Deck")
     async def deck(self, ctx):
         """Command grouping all card deck commands.
         Returns error to the channel is command is incomplete."""
         if ctx.invoked_subcommand is None:
-            await ctx.send(
-                f'Additional arguments required, see '
-                f'**{ctx.prefix}help deck** for available options.'
-            )
+            await ctx.send(f"Additional arguments required, see **{ctx.prefix}help deck** for available options.")
 
-    @deck.command(help='List available decks.', name='list')
+    @deck.command(help="List available decks.", name="list")
     async def list_decks(self, ctx):
-        decks = '\n'.join(self.deck_list)
-        await ctx.send(f'\nAvailable Decks:\n{decks}')
+        decks = "\n".join(self.deck_list)
+        await ctx.send(f"\nAvailable Decks:\n{decks}")
 
     @deck.command(help='Select a deck to draw cards from.')
     async def select(self, ctx, deck_name=None):
@@ -62,57 +59,38 @@ class DiceRoller(discord.Cog, name='Dice Roller'):
             )
         else:
             try:
-                self.card_list[ctx.guild.id] = os.listdir(f'./data/decks/{deck_name}')
+                self.card_list[ctx.guild.id] = os.listdir(f"./data/decks/{deck_name}")
                 self.active_deck[ctx.guild.id] = deck_name
-                await ctx.send(f'Active deck set to {self.active_deck[ctx.guild.id]}')
+                await ctx.send(f"Active deck set to {self.active_deck[ctx.guild.id]}")
             except FileNotFoundError:
                 await ctx.send(
                     f"ERROR: That deck doesn't exist.  Please select a deck from **{ctx.prefix}deck list**."
                 )
 
-    @deck.command(help='Draw a card from the selected deck')
+    @deck.command(help="Draw a card from the selected deck")
     async def draw(self, ctx):
         try:
             if len(self.card_list[ctx.guild.id]) == 0:
-                await ctx.send(
-                    f'No deck selected, please select a deck with **{ctx.prefix}deck select**.'
-                )
+                await ctx.send(f"No deck selected, please select a deck with **{ctx.prefix}deck select**.")
             else:
                 file_name = random.choice(self.card_list[ctx.guild.id])
                 self.card_list[ctx.guild.id].remove(file_name)
-                file = discord.File(f'./data/decks/{self.active_deck[ctx.guild.id]}/{file_name}')
+                file = discord.File(f"./data/decks/{self.active_deck[ctx.guild.id]}/{file_name}")
                 embed = deck_embed_template(file_name)
                 await ctx.send(embed=embed, file=file)
         except KeyError:
-            await ctx.send(
-                f'No deck selected, please select a deck with **{ctx.prefix}deck select**.'
-            )
+            await ctx.send(f"No deck selected, please select a deck with **{ctx.prefix}deck select**.")
 
-    @deck.command(help='Reset deck to full.')
+    @deck.command(help="Reset deck to full.")
     async def reset(self, ctx):
-        self.card_list[ctx.guild] = os.listdir(f'./data/decks/{self.active_deck[ctx.guild.id]}')
-        await ctx.send('Discards have been shuffled back into the deck.')
+        self.card_list[ctx.guild] = os.listdir(f"./data/decks/{self.active_deck[ctx.guild.id]}")
+        await ctx.send("Discards have been shuffled back into the deck.")
 
-    @deck.command(help='Rescan folder for new decks.')
+    @deck.command(help="Rescan folder for new decks.")
     @commands.has_guild_permissions(manage_guild=True)
     async def rescan(self, ctx):
-        self.deck_list = os.listdir('./data/decks')
-        await ctx.send(
-            f'Deck list has been refreshed.  Use **{ctx.prefix}deck list** to see all available card decks.'
-        )
-
-    if bot_config['dice_roller'] == 'array':
-
-        @tasks.loop(hours=1)
-        async def array_builder(self):
-            """Task loop to rebuild arrays every hour."""
-            self.rand_arrays = {
-                'd20': [random.randint(1, 20) for _ in range(1000)],
-                'd12': [random.randint(1, 12) for _ in range(1000)],
-                'd10': [random.randint(1, 10) for _ in range(1000)],
-                'd8': [random.randint(1, 8) for _ in range(1000)],
-                'd6': [random.randint(1, 6) for _ in range(1000)],
-            }
+        self.deck_list = os.listdir("./data/decks")
+        await ctx.send(f"Deck list has been refreshed.  Use **{ctx.prefix}deck list** to see all available card decks.")
 
     @roll.error
     async def cog_command_error(self, ctx, error):
--- cogs/Quotes.py
+++ cogs/Quotes.py
@@ -14,44 +14,51 @@ class Quotes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(help="Display a random quote submitted to the database, or one containing a search string.  "
-                         "Just like the internet, everything submitted is forever.")
+    @commands.group(
+        help="Display a random quote submitted to the database, or one containing a search string.  "
+        "Just like the internet, everything submitted is forever."
+    )
     async def quote(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.send(soulbot_db.quote_db_random(ctx.guild.id))
 
     @quote.group(name='add', help='Add a quote to the database.')
     async def add_quote(self, ctx, *, quote_text: str):
-        for _ in range(0, len(re.findall('@', quote_text))):
-            if len(re.findall('<@[!|&]\d+>', quote_text)) > 0:
-                mentioned_users = re.findall('<@[!|&]\d+>', quote_text)
+        for _ in range(0, len(re.findall("@", quote_text))):
+            if len(re.findall(r"<@[!|&]\d+>", quote_text)) > 0:
+                mentioned_users = re.findall(r"<@[!|&]\d+>", quote_text)
                 for member in mentioned_users:
-                    if '!' in member:
-                        member_id = member.replace('<', '').replace('>', '').replace('!', '').replace('@', '')
+                    if "!" in member:
+                        member_id = (
+                            member.replace("<", "")
+                            .replace(">", "")
+                            .replace("!", "")
+                            .replace("@", "")
+                        )
                         member_name = ctx.guild.get_member(int(member_id)).display_name
                     else:
                         member_name = ""
                     quote_text = quote_text.replace(member, member_name)
-            elif '@here' in quote_text or '@everyone' in quote_text:
-                quote_text = quote_text.replace('@', '')
+            elif "@here" in quote_text or "@everyone" in quote_text:
+                quote_text = quote_text.replace("@", "")
                 soulbot_db.quote_db_add(quote_text, ctx.guild.id)
         soulbot_db.quote_db_add(quote_text, ctx.guild.id)
         await ctx.send(f'Added "{str(quote_text)}" to quotes database.')
 
-    @quote.group(name='search', help='Search for a quote containing a specific term.')
+    @quote.group(name="search", help="Search for a quote containing a specific term.")
     async def quote_search(self, ctx, *, search_term: str):
         await ctx.send(soulbot_db.quote_db_search(search_term, ctx.guild.id))
 
-    @commands.command(help='Very Vahti like.', name='vahti', case_insensitive=True)
+    @commands.command(help="Very Vahti like.", name="vahti", case_insensitive=True)
     async def whale(self, ctx):
         response = requests.get(
-            'https://sv443.net/jokeapi/v2/joke/Pun'
-            '?blacklistFlags=nsfw,religious,political,racist,sexist',
-            headers={'Accept': 'application/json'},
+            "https://sv443.net/jokeapi/v2/joke/Pun"
+            "?blacklistFlags=nsfw,religious,political,racist,sexist",
+            headers={"Accept": "application/json"},
         )
         output = response.json()
-        if output['type'] == 'twopart':
-            await ctx.send(f'\n{output["setup"]}\n\n{output["delivery"]}')
+        if output["type"] == "twopart":
+            await ctx.send(f"\n{output['setup']}\n\n{output['delivery']}")
         else:
             await ctx.send(f'{output["joke"]}')
 
@@ -60,7 +67,7 @@ class Quotes(commands.Cog):
     @add_quote.error
     @quote_search.error
     async def cog_command_error(self, ctx, error):
-        await ctx.send(f'Experienced the following error:\n{error}')
+        await ctx.send(f"Experienced the following error:\n{error}")
 
 
 def setup(bot):
