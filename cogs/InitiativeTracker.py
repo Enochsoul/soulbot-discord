d2ae09bb02e65513ab2a51843dbbe4cd63341878
Jason S. <soulscode@gmail.com>
2025-12-24 12:25:05 -0700

    Correct the type hinting to be properly related to the used cog parent.

--- cogs/InitiativeTracker.py
+++ cogs/InitiativeTracker.py
@@ -261,7 +261,7 @@ class InitiativeTracker(discord.Cog, name="Initiative Tracker"):
     @attack.error
     @attack_npc.error
     @init_roll.error
-    async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
+    async def cog_command_error(self, ctx: discord.ApplicationContext, error: Exception) -> None:
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(f"Missing required argument. Use `{ctx.prefix}help {ctx.command}` for usage.")
         elif isinstance(error, commands.CommandInvokeError):
